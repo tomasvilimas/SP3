daf65303d08ee277e1440d42b22369f1be11e033
tomasvilimas <74533072+tomasvilimas@users.noreply.github.com>
2021-04-05 20:27:55 +0300

    noJoin

---
+++ page.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projektai</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        body {
+            margin: 0;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <div class="topnav" id="myTopnav">
+            <a href="/sp3/index.php">Projektai</a>
+            <a href="/sp3/page.php">Darbuotojai</a>
+            <div class=crud>
+                <h1>CRUD</h1>
+            </div>
+    </header>
+
+    </div>
+<?php 
+
+
+
+use Models\darbuotojas;
+
+require_once "bootstrap.php";
+
+
+function redirect_to_root(){
+    header("Location: " . parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH));
+}
+
+
+if(isset($_GET['name'])){
+    $product = new Models\Darbuotojas();
+    $product->setName($_GET['name']);
+    $entityManager->persist($product);
+    $entityManager->flush();
+    redirect_to_root();
+}
+
+
+if(isset($_GET['delete'])){
+    $user = $entityManager->find('Models\Darbuotojas', $_GET['delete']);
+    $entityManager->remove($user);
+    $entityManager->flush();
+    redirect_to_root();
+}
+
+
+if(isset($_POST['update_name'])){
+    $user = $entityManager->find('Models\Darbuotojas', $_POST['update_id']);
+    $user->setName($_POST['update_name']);
+    $entityManager->flush();
+    redirect_to_root();
+}
+
+
+$products = $entityManager->getRepository('Models\Darbuotojas')->findAll();
+print("<table>");
+foreach($products as $p)
+    print("<tr>" 
+            . "<td>" . $p->getId()  . "</td>" 
+            . "<td>" . $p->getName() . "</td>"
+            . "<td><a href=\"?delete={$p->getId()}\">DELETE</a>☢️</td>" 
+            . "<td><a href=\"?updatable={$p->getId()}\">UPDATE</a>♻️</td>"
+        . "</tr>");
+print("</table>"); 
+print("</pre><hr>");
+ 
+if(isset($_GET['updatable'])){
+    $product = $entityManager->find('Models\Darbuotojas', $_GET['updatable']);
+  
+    print("
+        <form action=\"\" method=\"POST\">
+            <input type=\"hidden\" name=\"update_id\" value=\"{$product->getId()}\">
+            <label  for=\"name\">Įveskite naują vardą: </label><br><br>
+            <input class=input type=\"text\" name=\"update_name\" value=\"{$product->getName()}\"><br><br>
+            <input class=button type=\"submit\" value=\"Pateikti\">
+        </form>
+    ");
+    print("<hr>");
+}
+
+
+?>
+<form action="" method="GET">
+  <label for="name"></label><br>
+  <input class=input type="text" name="name" value=""  placeholder="Darbuotojas vardas" Required><br>
+  <input class=button type="submit" value="Pridėti Darbuotoją">
+</form> 
+<hr>
+
+
+<footer>
+        <div id="footer-content">
+            <p> NO Copyright 2021 @ Tomas - NO Rights Reserved </p>
+        </div>
+    </footer>
--- src/models/darbuotojas.php
+++ src/models/darbuotojas.php
@@ -4,12 +4,11 @@ namespace Models;
 
 use Doctrine\ORM\Mapping as ORM;
 
-
 /**
  * @ORM\Entity
- * @ORM\Table(name="Projektai")
+ * @ORM\Table(name="Darbuotojai")
  */
-class Projektas
+class Darbuotojas
 {
     /** 
      * @ORM\Id
@@ -23,39 +22,36 @@ class Projektas
      */
     protected $name;
 
-    /**
-     * @ORM\ManyToOne(targetEntity="Darbuotojas")
-     * @ORM\JoinColumn(name="darbuotojo_id", referencedColumnName="id")
-     */
-    private $darbuotojas;
-
-
-    
-
-    public function getId(){
+    public function getId()
+    {
         return $this->id;
     }
 
-    public function getname(){
+    public function getname()
+    {
         return $this->name;
     }
 
-    
-    public function setName($name){
+
+    public function setName($name)
+    {
         $this->name = $name;
     }
 
-    public function getDarbuotojas(){
+
+
+    public function getDarbuotojas()
+    {
         return $this->name;
     }
 
-    
-    public function setDarbuotojas($name){
+
+    public function setDarbuotojas($name)
+    {
         $this->name = $name;
     }
 
-   
-    
+    public function __construct()
+    {
+    }
 }
-
-
